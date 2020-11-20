923cbfe6aa13d32abdae11b2e2696017f5a15c5a
KnightsUCF <peterk@knights.ucf.edu>
2020-11-20 16:56:04 -0500

    Create protein.py

---
+++ protein.py
@@ -0,0 +1,19 @@
+class Protein():
+
+
+    '''
+    records = Records()
+    file_path = 'sequences.fasta'
+    protein_records = records.GetAllProteinSequences(file_path)
+
+    DetermineSimilarity(protein_records)
+    '''
+    
+
+    def DetermineSimilarity(self, protein_records):
+        for protein in protein_records:
+        compare_to = protein_records[protein][0] # compare everything to the first protein record, can also compare to a selected sequence
+        print('Starting similarity analysis for protein: ', protein)
+        for record in protein_records[protein]:
+            how_similar = SequenceMatcher(None, compare_to, record).ratio()
+            print(how_similar)
