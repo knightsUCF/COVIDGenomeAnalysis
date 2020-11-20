bf4ab09fa2045050a269f90c60ce83161ae753f9
KnightsUCF <peterk@knights.ucf.edu>
2020-11-20 17:14:41 -0500

    Update protein.py

--- src/protein.py
+++ src/protein.py
@@ -1,4 +1,6 @@
 from difflib import SequenceMatcher
+from numba import jit
+
 
 
 class Protein():
@@ -12,10 +14,23 @@ class Protein():
     '''
 
     # warning: this can crash if computer specs are not enough to process 10 million lines in the complete record
+    # @jit(nopython=True)
     def DetermineSimilarity(self, protein_records):
         for protein in protein_records:
-        compare_to = protein_records[protein][0] # compare everything to the first protein record, can also compare to a selected sequence
-        print('Starting similarity analysis for protein: ', protein)
-        for record in protein_records[protein]:
-            how_similar = SequenceMatcher(None, compare_to, record).ratio()
-            print(how_similar)
+            compare_to = protein_records[protein][0] # compare everything to the first protein record, can also compare to a selected sequence
+            print('Starting similarity analysis for protein: ', protein)
+            for record in protein_records[protein]:
+                how_similar = SequenceMatcher(None, compare_to, record).ratio()
+                print(how_similar)
+
+
+'''
+records = Records()
+protein = Protein()
+
+
+file_path = 'sequences.fasta'
+protein_records = records.GetAllProteinSequences(file_path)
+
+protein.DetermineSimilarity(protein_records)
+'''
