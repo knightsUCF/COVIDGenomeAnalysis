cf2e7a230c2cf977e301f796baa2b06d74c19f1f
KnightsUCF <peterk@knights.ucf.edu>
2020-11-20 03:06:46 -0500

    Update records.py

--- records.py
+++ records.py
@@ -4,6 +4,7 @@ from Bio import SeqIO
 
 database = {}
 
+
 # this is the entire protein sequence collection of all records in one fasta file, 500mb+, 10 million lines of data, needs to be downloaded off the site
 for record in SeqIO.parse("data/sequences.fasta", "fasta"):
     description_words = record.description.split()
@@ -11,7 +12,6 @@ for record in SeqIO.parse("data/sequences.fasta", "fasta"):
         database[description_words[1][1:]] = []
     sequence = record.seq
     database[description_words[1][1:]].append(str(sequence))
-    
     
  
 for i in database:
