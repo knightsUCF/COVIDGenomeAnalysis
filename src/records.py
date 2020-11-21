3b33c209d4bfa467c09ef87becff53041e91bc06
KnightsUCF <peterk@knights.ucf.edu>
2020-11-21 14:57:42 -0500

    Update records.py

--- src/records.py
+++ src/records.py
@@ -8,11 +8,11 @@ class Records():
 
     def GetAllProteinSequences(self, file_path):
         print('Categorizing protein sequences...\n')
-        database = {}
+        proteins = {}
         for record in SeqIO.parse(file_path, "fasta"):
-            description_words = record.description.split()
-            if description_words[1][1:] not in database:
-                database[description_words[1][1:]] = []
+            labels = record.description.split()
+            if labels[1][1:] not in proteins:
+                proteins[labels[1][1:]] = []
             sequence = record.seq
-            database[description_words[1][1:]].append(str(sequence))
-        return database
+            proteins[labels[1][1:]].append(str(sequence))
+        return proteins
